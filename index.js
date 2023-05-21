0f6060283644d5a164e206f400ae842c303a02b9
unknown <harissh.a.s@gmail.com>
2023-05-21 16:14:21 +0530

    included local time reference

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ client.on("message",function(topic, message, packet)
     
     latestmessage=message;
 
-    let date_ob = new Date();
+    let date_ob_str = new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" });
+    let date_ob = new Date(date_ob_str);
     let date = ("0" + date_ob.getDate()).slice(-2);
     let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
     let year = date_ob.getFullYear();
